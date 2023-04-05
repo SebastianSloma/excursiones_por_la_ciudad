439b9c325ed2ba661999f2617295857dba1c5eb7
Sebastian <sebastiansloma@gmail.com>
2023-04-05 12:54:41 +0200

    added city list

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,22 @@
-import React,{Component} from 'react'
-import './TourList.css'
+import React, { Component } from 'react';
+import './TourList.css';
+import Tour from '../Tour/Tour';
+import { tourData } from '../tourData';
 
-export default class TourList extends Component{
-    render(){
-        return(
-            <section>
-                tour
-            </section>
-        )
-    }
+export default class TourList extends Component {
+	state = {
+		tours: tourData,
+	};
+	render() {
+		const { tours } = this.state;
+		return (
+			<section className='tourlist'>
+				{tours.map(tour =>{
+                    return(
+                        <Tour key={tour.id} tour={tour}/>
+                    )
+                })}
+			</section>
+		);
+	}
 }
